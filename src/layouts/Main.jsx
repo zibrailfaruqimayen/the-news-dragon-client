7570c4cacb7bc2ffc31797400a96c06dd8302836
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-18 03:50:51 +0600

    file setup

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "../pages/Shared/Header/Header";
+import Footer from "../pages/Shared/Footer/Footer";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Main = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg={3}>sm=true</Col>
+          <Col lg={6}>
+            <h2>Main content coming</h2>
+          </Col>
+          <Col lg={3}>sm=true</Col>
+        </Row>
+      </Container>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeftNav = () => {
+  return (
+    <div>
+      <h2>this is left Nav</h2>
+    </div>
+  );
+};
+
+export default LeftNav;
