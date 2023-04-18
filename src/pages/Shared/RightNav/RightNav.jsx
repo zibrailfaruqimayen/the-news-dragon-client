d99f867f740dcdbc2c7fbbb520bc32382c46716b
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-18 16:42:42 +0600

    rightnav added

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import "./RightNav.css";
+import QZone from "../QZone/QZone";
+import bg from "../../../assets/bg.png";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>right nav</h2>
+      <h4>Login With</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub />
+        Login with Github
+      </Button>
+      <div>
+        <h4>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram /> Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone />
+      <div className="position-relative add-img text-center">
+        <img className="w-100" src={bg} alt="" />
+        <div className="position-absolute top-50 start-50 translate-middle text-white">
+          <h3 className="fs-1 text-center">Create an Amazing Newspaper</h3>
+          <p className="text-center">
+            Discover thousands of options, easy to customize layouts, one-click
+            to import demo and much more.
+          </p>
+          <Button variant="danger">Learn More</Button>
+        </div>
+      </div>
     </div>
   );
 };
