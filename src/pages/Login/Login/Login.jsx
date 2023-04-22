292eabf858e938f85f0aba29485baa117d4235f8
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-23 01:57:16 +0600

    login and register form added

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
+    <Container className="w-25 mx-auto">
       <h2>Please Login here</h2>
-    </div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+
+        <Button className="mb-3" variant="primary" type="submit">
+          Login
+        </Button>
+        <br />
+        <Form.Text className="text-muted">
+          Dont't Have An Account ? <Link to="/register">Register</Link>
+        </Form.Text>
+        <Form.Text className="text-success"></Form.Text>
+        <Form.Text className="text-danger"></Form.Text>
+      </Form>
+    </Container>
   );
 };
 
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,68 @@
 import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h2>Please Sign Up Here</h2>
-    </div>
+    <Container className="w-25 mx-auto">
+      <h2>Register your account</h2>
+      <hr />
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Enter Your Name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control
+            type="text"
+            name="photo"
+            placeholder="Enter Your Photo URL"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group
+          className="mb-3 text-secondary"
+          controlId="formBasicCheckbox"
+        >
+          <Form.Check type="checkbox" label="Accept Term & Conditions" />
+        </Form.Group>
+
+        <Button className="mb-3" variant="primary" type="submit">
+          Register
+        </Button>
+        <br />
+        <Form.Text className="text-muted">
+          Already Have An Account ? <Link to="/login">Login</Link>
+        </Form.Text>
+        <Form.Text className="text-success"></Form.Text>
+        <Form.Text className="text-danger"></Form.Text>
+      </Form>
+    </Container>
   );
 };
 
