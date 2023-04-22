b6fd760c5ea0d5b5a8644b666ec8b7ac240f3d86
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-23 01:03:47 +0600

    take a dinner

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -19,8 +19,8 @@ const NavigationBar = () => {
         <Container>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mx-auto">
-              <Link to="/">Home</Link>
+            <Nav className="mx-auto navigatin-bar">
+              <Link to="/category/0">Home</Link>
 
               <Nav.Link href="#pricing">About</Nav.Link>
               <Nav.Link href="#pricing">Career</Nav.Link>
