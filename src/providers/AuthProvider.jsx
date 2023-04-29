61096feffb45a15eecbe0fe2444b94e7d7c55900
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-29 14:12:22 +0600

    authentication

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext } from "react";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -7,7 +7,12 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const user = null;
-  const authInfo = { user };
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = { user, createUser };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
